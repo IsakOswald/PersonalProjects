2ddeb33a4f75fe9d7ec8f315db2a03e2fcbc5922
IsakOswald <isak.oswald.cs@gmail.com>
2025-05-23 19:45:36 +1000

    added SQL support

---
+++ nvim/lua/plugins/dadbod.lua
@@ -0,0 +1,8 @@
+return {
+    "tpope/vim-dadbod",
+    "kristijanhusak/vim-dadbod-completion",
+    "kristijanhusak/vim-dadbod-ui",
+}
+
+--NOTE: This is for SQL. Can remove if i dont need.
+--WARNING: Need to still set up completions via dadbob-completions in autocomplete.lua (later)
