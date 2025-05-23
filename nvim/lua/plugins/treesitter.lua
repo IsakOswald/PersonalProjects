dc0beec35f4b12cc6f077d95b5ca58cf25144496
Isak Kurt Oswald <isak.oswald.cs@gmail.com>
2025-05-23 16:11:49 +1000

    Update treesitter.lua

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -25,7 +25,7 @@ return {
 			auto_install = false, -- no need to auto-install stuff you don't want
 			highlight = {
 				enable = true,
-				additional_vim_regex_highlighting = {},
+				additional_vim_regex_highlighting = false,
 			},
 			indent = {
 				enable = true,
