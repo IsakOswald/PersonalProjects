cffa17e1b5e2d72765588e69e15604f72c99297a
IsakOswald <isak.oswald.cs@gmail.com>
2025-06-18 17:03:07 +1000

    Change colortheme

--- nvim/Lightweight/nvim/lua/plugins/colortheme.lua
+++ nvim/Lightweight/nvim/lua/plugins/colortheme.lua
@@ -22,14 +22,12 @@
 --}
 
 return {
-    "loctvl842/monokai-pro.nvim",
-    lazy = false,                    -- load immediately
-    priority = 1000,                 -- make sure it's the first colorscheme
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
     config = function()
-        vim.opt.termguicolors = true -- ensure true color is on before anything
-        require("monokai-pro").setup({
-            filter = "octagon",      -- or "octagon", "machine", etc.
-        })
-        vim.cmd("colorscheme monokai-pro") -- apply the colorscheme
+        vim.opt.termguicolors = true          -- Ensure true color is enabled
+        vim.g.gruvbox_material_palette = "material" -- Choose your preferred palette
+        vim.cmd("colorscheme gruvbox-material") -- Apply the colorscheme
     end,
 }
